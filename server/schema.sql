225571691ff29d778672ae71f73ba1bbec2da45e
davidwilson3200 <david.wilson3200@gmail.com>
2020-04-15 21:47:30 -0700

    finish the schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,10 +2,16 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
   /* Describe your table here.*/
+  /* may need to include a time-sent column for mrssages*/
+CREATE TABLE messages (
+
+  id integer primary key, user-id text, text text, roomname text, foreign key (user-id) references users (id)
 );
 
+CREATE TABLE users (
+  id integer primary key, username text
+);
 /* Create other tables and define schemas for them here! */
 
 
