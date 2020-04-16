00f26289d4e5dcf0d1198402cf4384d2975fe862
davidwilson3200 <david.wilson3200@gmail.com>
2020-04-15 22:07:25 -0700

    we wrote some sedo code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,13 +3,56 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
+    //like the http handler from asynchronous swim sprint
+    //retrieves the messages data that we retrieved from the models/index.js file
+    //then writes the information
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
+    // what information do we need to send back when a get request for a user is sent? : send back all the messages that the user sent
     get: function (req, res) {},
     post: function (req, res) {}
   }
 };
+
+// module.exports.router = (req, res, next = () => { }) => {
+//   const produceImage = function (err, data) {
+//     //console.log('Reached callback');
+//     //console.log(data);
+//     if (err) {
+//       res.writeHead(404, headers);
+//       res.end();
+//     } else {
+//       res.writeHead(200, headers);
+//       // res.end();
+//       res.end(data, 'binary')
+//     }
+//   };
+
+//   console.log('Serving request type ' + req.method + ' for url ' + req.url);
+//   console.log(req.url);
+//   if (req.method === 'OPTIONS') {
+//     res.writeHead(200, headers);
+//     res.end('');
+//   }
+
+//   if (req.method === 'GET') { //GET
+//     // if // the designated URL is just the server URL, send a swim command message (like below)
+//     if (req.url === '/') { //http://localhost:8080/
+//       //     '/' || '/background.jpg'
+//       res.writeHead(200, headers);
+//       res.end(messageQueue.dequeue());
+//     } else if (req.url === '/background.jpg') {
+//       fs.readFile(module.exports.backgroundImageFile, produceImage);
+//     }
+//   }
+
+//   if (req.method === 'POST') {
+//     res.writeHead(201, headers);
+//     res.end();
+//   }
+//   next();
+// }
 
--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,7 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {}, // a function which produces all the messages
+    //this one retrieves all the messages from the database
     post: function () {} // a function which can be used to insert a message into the database
   },
 
