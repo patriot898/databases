4efbb9b076b78041dd1568d4c0d2317543f91803
davidwilson3200 <david.wilson3200@gmail.com>
2020-04-16 12:32:12 -0700

    complete the model part

--- server/models/index.js
+++ server/models/index.js
@@ -1,16 +1,96 @@
 var db = require('../db');
+var connection = db.connection;
+
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (callback) {
+     var getMessageQuery = 'select * from messages';
+
+      connection.query(getMessageQuery,(err, result) =>{
+       if (err) {
+         callback(err);
+       } else {
+         callback(err, result);
+       }
+      });
+    }, // a function which produces all the messages
     //this one retrieves all the messages from the database
-    post: function () {} // a function which can be used to insert a message into the database
+
+
+
+
+
+    post: function (message, callback) {
+
+
+     var writeMessageQuery = 'INSERT INTO message (userid, text, roomname) values ((SELECT userid FROM users WHERE username = "' + message.username + '"), "' + message.text + '", "' + message.roomname + '")';
+
+     connection.query(writeMessageQuery,(err, result) =>{
+      if (err) {
+        callback(err);
+      } else {
+        callback(err, result);
+      }
+     });
+
+
+
+
+    } // a function which can be used to insert a message into the database
+
+
+    // INSERT INTO table_listnames (name, address, tele)
+    // SELECT * FROM (SELECT 'Rupert', 'Somewhere', '022') AS tmp
+    // WHERE NOT EXISTS (
+    //     SELECT name FROM table_listnames WHERE name = 'Rupert'
+    // ) LIMIT 1;
+
+    // CREATE TABLE messages (
+
+    //   id integer primary key, userid text, text text, roomname text, foreign key (userid) references users (id)
+    // );
+
+
+
+
+
+
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (username, callback) {
+      var query = 'SELECT * FROM messages WHERE userid = (SELECT id FROM users WHERE username = "' + username + '")';
+    },
+
+    post: function (username) {
+      var userQuery = 'INSERT INTO users (username) VALUES ("' + username + '")';
+
+      connection.query(userQuery,(err, result) =>{
+        if (err) {
+          callback(err);
+        } else {
+          callback(err, result);
+        }
+       });
+    }
   }
 };
+
+
+
+//creates an object which has a connection to the mysql database
+//use the .query() method to execute mysql commands
+//example:
+// con.query("CREATE DATABASE mydb", function (err, result) {
+//   if (err) throw err;
+//   console.log("Database created");
+
+//example to create a table:
+//var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Table created");
+  // });
 
